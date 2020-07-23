b5f574f7cb73c65d2ca2002ed52c6bcc9891f452
Emelie Sundgren <emelie.sundgren@hotmail.com>
2020-07-23 14:32:14 +0200

    added styling + footer

---
+++ script.js
@@ -0,0 +1,11 @@
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//     }
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById('nav').style.height = 15vh;
+//     // document.getElementById("logo").style.fontSize = "25px";
+//   } else {
+//     document.getElementById('nav').style.height = 30vh;
+//     // document.getElementById("logo").style.fontSize = "35px";
+//   }
